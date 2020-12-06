0bb8dd783114f653e2a26a803c0931d21927f2f9
HiroyukiYagihashi <h.yagihashi2424@icloud.com>
2020-12-07 01:31:30 +0900

    Update README

---
+++ README.md
@@ -0,0 +1,24 @@
+## ☁️ souki(想起)
+souki is a state management library for React apps.
+
+## Getting stated
+### Installing
+
+Install souki by npm.
+```shell script
+npm i souki
+```
+
+## Contributing
+Please read CONTRIBUTING.md for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+We use [SemVer](https://semver.org) for versioning. For the versions available, see the tags on this repository.
+
+## Authors
+- [Hiroyuki Yagihashi](https://github.com/HiroyukiYagihashi)
+
+See also the list of contributors who participated in this project.
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,8 @@
 import { Dispatch, SetStateAction, useContext, useState, createContext } from "react";
 import * as React from "react";
 
-// TODO: Inject outside
-// interface SoukiState {
-//   value: number;
-// }
-
-// const initialState: object = {
-//   value: 0,
-// };
-
-const SoukiContext = createContext<object>({});
-const SetSoukiContext = createContext<Dispatch<SetStateAction<object>>>(() => {});
+const SoukiContext = createContext<any>({});
+const SetSoukiContext = createContext<Dispatch<SetStateAction<any>>>(() => {});
 
 export function useSouki() {
   return useContext(SoukiContext);
@@ -21,10 +12,10 @@ export function useSetSouki() {
 }
 
 export function SoukiProvider(props: {
-  initialState: object;
+  initialState: any;
   children: React.ReactNode;
 }) {
-  const [state, setState] = useState<object>(props.initialState);
+  const [state, setState] = useState<any>(props.initialState);
 
   return (
     <SoukiContext.Provider value={state}>
